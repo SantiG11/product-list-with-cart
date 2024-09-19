295ec385ea41508a7920475c5f8ab8ff805dd80f
Santiago Gomez <Santigomez1129@gmail.com>
2024-09-19 20:41:28 -0300

    Added the addToCart, hideOrShowEl, displayAmount, activateBtn, addCartItem, increment and decrement functions. Added all variables needed for those functions to work properly. Next I should work on updating the cart items, the total order price, and the remove item functions.

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ const products = document.querySelectorAll('.product-card');
 const totalItemsAmount = document.getElementById('total-items-amount');
 const cartContainer = document.querySelector('.product-cart-container');
 const cartImg = document.querySelector('.cart-img');
-const cartPlaceHolder = document.querySelector('.cart-placeholder')
+const cartPlaceHolder = document.querySelector('.cart-placeholder');
 const orderTotalCont = document.querySelector('.order-total-container');
 const orderTotal = document.getElementById('order-total');
 const confirmBtn = document.querySelector('.confirm-order-btn');
 const carbonNeutral = document.querySelector('.carbon-neutral-container');
-
 
 let itemsAmount = 0;
 let orderTotalRes = 0;
@@ -38,53 +37,11 @@ subQuantity() {
 
 const productObjets = [];
 
-products.forEach(product => {
-    // Use a unique identifier for each product;
-    
-    const productNameEl = product.querySelector('.product-name');
-    const productName = productNameEl.textContent;
-    const productId = productName;
-    const productDescription = product.querySelector('.product-description').textContent;
-    const productPrice = product.querySelector('.product-price').textContent;
-    const priceNumber = parseFloat(productPrice.replace('$', '').trim());
-    const addBtn = product.querySelector('.add-btn');
+const displayAmount = () => {
+    totalItemsAmount.textContent = `(${itemsAmount})`;
+}
 
-    const productObj = new ProductObj (
-            productName, productId, productDescription, priceNumber
-    );
-
-   
-
-    productObjets.push(productObj);
-
-    addBtn.addEventListener('click', (e) => {
-        if (itemsAmount === 0) {
-            hideOrShowEl(orderTotalCont);
-            hideOrShowEl(cartImg);
-            hideOrShowEl(cartPlaceHolder);
-            hideOrShowEl(carbonNeutral);
-            hideOrShowEl(confirmBtn);
-        }
-        itemsAmount++;
-
-
-        displayAmount();
-        // addProduct( productId, productDescription, productPrice, priceNumber);
-        activateBtn(product, productId, priceNumber, addBtn);
-
-
-
-    });
-});
-
-const productToFind = 'Waffle';
-
-productObjets.find(product => product.name === productToFind).addQuantity();
-productObjets.find(product => product.name === productToFind).addQuantity();
-productObjets.find(product => product.name === productToFind).addQuantity();
-productObjets.find(product => product.name === productToFind).subQuantity();
-
-console.log(productObjets.find(product => product.name === productToFind));
+window.onload = displayAmount;
 
 const hideOrShowEl = (element) => {
     if (element.classList.contains('hidden')) {
@@ -97,140 +54,44 @@ const hideOrShowEl = (element) => {
 }
 
 
-const displayAmount = () => {
-    totalItemsAmount.textContent = `(${itemsAmount})`;
-}
-
-
-const updateOrderTotal = (num) => {
-    orderTotalRes = orderTotalRes + num;
-    orderTotal.textContent = '$' + orderTotalRes.toFixed(2);
-}
-
-const deleteItem = (e) => {
-    let item = e.target.parentElement;
-    let itemId = item.id
-    let deletedAmount = item.querySelector('.product-amount').textContent;
-    let deletedTotalItemPrice = item.querySelector('.total-item-price').textContent;
-    let deletedPriceNumber = parseFloat(deletedTotalItemPrice.replace('$', '').trim());
-    
-
-
-    if (parseInt(deletedAmount)) {
-        itemsAmount = itemsAmount - parseInt(deletedAmount);
-    } else {
-        itemsAmount -= 1
-    }
-    updateOrderTotal(-deletedPriceNumber);
+const increment = (productSelected, quantityP) => {
+    itemsAmount++;
     displayAmount();
-    productMap.delete(item.id);
-    item.remove();
+    productSelected.addQuantity();
+    quantityP.textContent = productSelected.quantity;
+}
+
+const decrement = (productSelected, quantityP, el, activeBtn) => {
+    itemsAmount--;
+
     if (itemsAmount === 0) {
-        
         hideOrShowEl(orderTotalCont);
         hideOrShowEl(cartImg);
         hideOrShowEl(cartPlaceHolder);
         hideOrShowEl(carbonNeutral);
         hideOrShowEl(confirmBtn);
     }
-}
-
-
-const createItemEl = (product, quantity, itemAdded, productId, productDescription, productPrice, priceNumber) => {
-
-    
- 
-    itemAdded = document.createElement('div');
-    itemAdded.classList.add('cart-item');
-    itemAdded.id = productId;
-
-    const itemAddedDescription = document.createElement('h4');
-    const itemAddedPrice = document.createElement('p');
-    const totalItemPrice = document.createElement('p');
-    const itemAddedAmount = document.createElement('label');
-    const deleteBtn = document.createElement('img');
-
-    deleteBtn.src = 'assets/images/icon-remove-item.svg';
-    deleteBtn.alt = 'Remove item icon';
-
-    itemAdded.appendChild(itemAddedDescription);
-    itemAdded.appendChild(itemAddedAmount);
-    itemAdded.appendChild(itemAddedPrice);
-    itemAdded.appendChild(totalItemPrice);
-    itemAdded.appendChild(deleteBtn);
-
-    itemAddedAmount.classList.add('product-amount');
-    totalItemPrice.classList.add('total-item-price');
-    deleteBtn.classList.add('delete-btn');
-
-    deleteBtn.addEventListener('click', (e) => {
-        deleteItem(e);
+    displayAmount();
+    productSelected.subQuantity();
+    if(!productSelected.quantity){
+        hideOrShowEl(el);
         
-        
-    });
-    // Store reference in the map
-    productMap.set(productId, { element: itemAdded, amount: 1 });
-
-    itemAddedDescription.textContent = productDescription;
-    itemAddedAmount.textContent = '1x';
-    itemAddedPrice.textContent = "@" + productPrice;
-    totalItemPrice.textContent = `$${priceNumber.toFixed(2)}`;
-    updateOrderTotal(priceNumber)
-    // For easier querying later
-
-    cartContainer.appendChild(itemAdded);
-    displayAmount()
-
-}
-
-const updateProductEl = (productId, priceNumber, quant) => {
-    const { element, amount } = productMap.get(productId);
-    const newAmount = amount + quant;
-
-    const totalItemPriceElement = element.querySelector('.total-item-price');
-    const amountElement = element.querySelector('.product-amount');
-    const totalItemPrice = parseFloat((newAmount * priceNumber).toFixed(2))
-
-    amountElement.textContent = `${newAmount}x`;
-    totalItemPriceElement.textContent = `$${totalItemPrice.toFixed(2)}`;
-
-    if (quant >= 0) {
-        updateOrderTotal(priceNumber);
-    } else {
-        updateOrderTotal(-priceNumber);
+        hideOrShowEl(activeBtn);
     }
 
+    quantityP.textContent = productSelected.quantity;
+}
 
+const activateBtn = (productSelected,product, el) => {
+
+    const cartEl = document.getElementById(productSelected.id);
     
-}
-
-
-const addProduct = (product, quantity, productId, productDescription, productPrice, priceNumber) => {
-    let itemAdded = productMap.get(productId);
-
-
-    if (!itemAdded) {
-        // Create a new product entry
-        createItemEl(product, quantity, itemAdded, productId, productDescription, productPrice, priceNumber)
-
-    }
-}
-
-window.onload = displayAmount;
-
-
-
-
-const activateBtn = (quantity, product, productId, priceNumber, el) => {
-
-    const cartEl = document.getElementById(productId);
-
     const activeBtn = document.createElement('button');
     activeBtn.classList.add('add-btn', 'active');
 
     const quantityP = document.createElement('p');
     
-    quantityP.textContent = quantity;
+    quantityP.textContent = productSelected.quantity;
 
     const decrementDiv = document.createElement('div');
     const decrementImg = document.createElement('img');
@@ -239,33 +100,6 @@ const activateBtn = (quantity, product, productId, priceNumber, el) => {
     decrementImg.classList.add('decrement-btn');
     decrementDiv.appendChild(decrementImg);
 
-    decrementDiv.addEventListener('click', () => {
-
-
-        updateProductEl(productId, priceNumber, -1);
-
-        
-
-        quantity--;
-        
-        
-        quantityP.textContent = quantity;
-        if (quantity < 1) {
-            const deleteBtn = cartEl.querySelector('.delete-btn')
-
-            hideOrShowEl(activeBtn);
-            hideOrShowEl(el);
-
-
-            deleteBtn.click();
-
-        } else {
-            itemsAmount--
-            displayAmount();
-        }
-        
-    })
-
     const incrementDiv = document.createElement('div');
     const incrementImg = document.createElement('img');
     incrementImg.src = 'assets/images/icon-increment-quantity.svg';
@@ -273,15 +107,14 @@ const activateBtn = (quantity, product, productId, priceNumber, el) => {
     incrementImg.classList.add('increment-btn');
     incrementDiv.appendChild(incrementImg);
 
-    incrementDiv.addEventListener('click', () => {
-
-        updateProductEl(productId, priceNumber, 1);
-        quantity++;
-        itemsAmount++;
-        quantityP.textContent = quantity;
-        displayAmount();
+    decrementDiv.addEventListener('click', () => {
+        decrement(productSelected, quantityP, el, activeBtn);
     })
 
+    incrementDiv.addEventListener('click', () => {
+        increment(productSelected, quantityP);
+    })
+    
 
     activeBtn.appendChild(decrementDiv);
     activeBtn.appendChild(quantityP);
@@ -290,6 +123,78 @@ const activateBtn = (quantity, product, productId, priceNumber, el) => {
     product.insertBefore(activeBtn, el);
     hideOrShowEl(el)
 }
+
+const addCartItem = (productSelected) => {
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.id = 'cart-item-' + productSelected.id;
+
+    const cartItemDescription = document.createElement('h4');
+    const cartItemPrice = document.createElement('p');
+    const totalItemPrice = document.createElement('p');
+    const cartItemAmount = document.createElement('label');
+    const deleteBtn = document.createElement('img');
+
+    deleteBtn.src = 'assets/images/icon-remove-item.svg';
+    deleteBtn.alt = 'Remove item icon';
+
+    cartItem.appendChild(cartItemDescription);
+    cartItem.appendChild(cartItemAmount);
+    cartItem.appendChild(cartItemPrice);
+    cartItem.appendChild(totalItemPrice);
+    cartItem.appendChild(deleteBtn);
+
+    cartItemAmount.classList.add('product-amount');
+    totalItemPrice.classList.add('total-item-price');
+    deleteBtn.classList.add('delete-btn');
+
+    cartItemDescription.textContent = productSelected.description;
+    cartItemAmount.textContent = productSelected.quantity + 'x';
+    cartItemPrice.textContent = "@" + productSelected.price;
+    totalItemPrice.textContent = `$${productSelected.price}`;
+
+    cartContainer.appendChild(cartItem);
+}
+
+
+const addToCart = (productName, product, addBtn) => {
+    if (itemsAmount === 0) {
+        hideOrShowEl(orderTotalCont);
+        hideOrShowEl(cartImg);
+        hideOrShowEl(cartPlaceHolder);
+        hideOrShowEl(carbonNeutral);
+        hideOrShowEl(confirmBtn);
+    }
+    const productSelected = productObjets.find(product => product.name === productName);
+    productSelected.addQuantity();
+    itemsAmount++;
+    displayAmount();
+    activateBtn(productSelected, product, addBtn);
+    addCartItem(productSelected);
+}
+
+products.forEach(product => {
+
+    const productNameEl = product.querySelector('.product-name');
+    const productName = productNameEl.textContent;
+    const productDescription = product.querySelector('.product-description').textContent;
+    const productPrice = product.querySelector('.product-price').textContent;
+    const priceNumber = parseFloat(productPrice.replace('$', '').trim()).toFixed(2);
+    const addBtn = product.querySelector('.add-btn');
+
+    const productObj = new ProductObj (
+            productName, productName, productDescription, priceNumber
+    );
+
+    productObjets.push(productObj);
+
+    addBtn.addEventListener('click', () => {
+        addToCart(productName, product, addBtn);
+    });
+
+});
+
+
 
 
 
